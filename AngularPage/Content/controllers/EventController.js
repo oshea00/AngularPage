ca0f5ebaa3d14e761504bdaa68b6733a29f10804
Mike OShea <oshea00@gmail.com>
2013-07-17 15:58:03 -0700

    added data service

--- AngularPage/Content/controllers/EventController.js
+++ AngularPage/Content/controllers/EventController.js
@@ -9,42 +9,11 @@ eventsApp.filter('firstupper', function () {
 
 
 eventsApp.controller('EventController',
-    function EventController($scope) {
+    function EventController($scope,eventData) {
 
         $scope.sortOrder = 'name';
         $scope.query = '';
-        $scope.event = {
-            name: "Angular Boot Camp",
-            date: new Date(),
-            time: '10:30 am',
-            rating: 3.2,
-            location: {
-                address: 'Google Headquarters',
-                city: 'Mountain View',
-                province: 'CA'
-            },
-            imageUrl: '/Content/images/russell_logo.gif',
-            sessions: [
-                {
-                    name: 'Directives Masterclass Beginner',
-                    creatorName: "Mike O'Shea",
-                    duration: '1 hour',
-                    level: 'advanced',
-                    abstract: 'This is a sample description for the session abstract',
-                    upVoteCount: 10
-                },
-                {
-                    name: 'scopes for fun and profit',
-                    level: 'beginner',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'well-behaved Controllers',
-                    level: 'advanced',
-                    upVoteCount: 0
-                }
-            ]
-        }
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
---
+++ AngularPage/Content/services/EventData.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+eventsApp.factory('eventData', function () {
+    return {
+        event: {
+            name: "Angular Boot Camp",
+            date: new Date(),
+            time: '10:30 am',
+            rating: 3.2,
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/Content/images/russell_logo.gif',
+            sessions: [
+                {
+                    name: 'Directives Masterclass Beginner',
+                    creatorName: "Mike O'Shea",
+                    duration: '1 hour',
+                    level: 'advanced',
+                    abstract: 'This is a sample description for the session abstract',
+                    upVoteCount: 10
+                },
+                {
+                    name: 'scopes for fun and profit',
+                    level: 'beginner',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'well-behaved Controllers',
+                    level: 'advanced',
+                    upVoteCount: 0
+                }
+            ]
+        }
+    };
+});
